d3cf6c12ae949429fab58b8d1d7275a96f4813d5
Annie Truex <artruex@gmail.com>
2019-08-13 19:17:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ class TicTacToe
     display_board
   end
   
-    def won?
+  def won?
     WIN_COMBINATIONS.any? do |combo|
       if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
         return combo
